83236f3489aee272e52c7244b9196126e7884420
welion <353566165@qq.com>
2015-04-08 11:56:41 +0800

    First init

---
+++ nmap/nmap.py
@@ -0,0 +1,6 @@
+PyCrust 0.9.8 - The Flakiest Python Shell
+Python 2.7.3 (default, Jan  2 2013, 16:53:07) 
+[GCC 4.7.2] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import nmap
+>>> 
